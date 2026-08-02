4678ee3f2be34441e96f5e3b272bce8ad1858040
miffy-Laptop <kkian481718@gmail.com>
2026-08-02 23:18:48 +0800

    feat: add post-start script to .devcontainer configuration

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,5 +5,5 @@ git pull
 pip install -r requirements.txt
 
 # 使用迴圈讓小水獺掛掉或收到 !restart 指令時可以自動重啟
-nohup bash -c 'while true; do python3 main.py; echo "Bot crashed or restarted, restarting in 3 seconds..."; sleep 3; done' > bot.log 2>&1 < /dev/null &
+setsid bash -c 'while true; do echo "Starting Bot..."; python3 -u main.py; echo "Bot crashed or restarted, restarting in 3 seconds..."; sleep 3; done' > bot.log 2>&1 < /dev/null &
 
