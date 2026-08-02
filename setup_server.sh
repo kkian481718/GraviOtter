f1ff73de61a49aa165fd2a9b71987daccc155e85
miffy-Laptop <kkian481718@gmail.com>
2026-08-02 22:45:19 +0800

    chore: remove setup_server.sh as we moved to Codespaces

--- setup_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install -y python3-pip python3-venv npm tmux
-sudo npm install -g pm2
-cd ~/GraviOtter
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
